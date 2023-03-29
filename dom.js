731c4a7a2b9335e57220a7541eba9e7f7997ccc8
8562917856 <129104680+8562917856@users.noreply.github.com>
2023-03-29 15:34:38 +0530

    getelementbytahname

--- dom.js
+++ dom.js
@@ -1,11 +1,14 @@
-var items = document.getElementsByClassName('list-group-item');
-console.log(items)
-console.log(items[1]);
-// items[0].style.fontWeight='bold'
-// items[1].style.fontWeight='bold'
-// items[2].style.fontWeight='bold'
-// items[3].style.fontWeight='bold'
-items[2].style.backgroundColor='green'
-for(var i=0; i<items.length; i++){
-    items[i].style.fontWeight='bold'
+// var items = document.getElementsByClassName('list-group-item');
+// console.log(items)
+// console.log(items[1]);
+// items[2].style.backgroundColor='green'
+// for(var i=0; i<items.length; i++){
+//     items[i].style.fontWeight='bold'
+// }
+var items = document.getElementsByTagNameNS('li');
+console.log(li)
+console.log(li[1]);
+li[2].style.backgroundColor='green'
+for(var i=0; i<li.length; i++){
+    li[i].style.fontWeight='bold'
 }
